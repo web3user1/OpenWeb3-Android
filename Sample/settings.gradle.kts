37cd62ddae8761fc803fc26e33891ab6a6eef0b6
biu <biu@mtsocialdao.com>
2025-03-13 17:34:39 +0800

    1.0.7-fixed

--- Sample/settings.gradle.kts
+++ Sample/settings.gradle.kts
@@ -17,7 +17,6 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
         maven {
-            //url = uri("${rootProject.projectDir}/../aar/1.0.0")
             url = uri("https://raw.githubusercontent.com/web3user1/OpenWeb3-Android/main/aar/1.0.7") // 本地 Maven 仓库路径
         }
     }
